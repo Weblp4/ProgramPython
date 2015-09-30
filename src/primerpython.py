efd4996740bd3c1869d77f000dd7d636391ba9bf
EjemploLP4 <riascosarmas@gmail.com>
2015-09-29 23:10:40 -0500

    Primer commit de python

---
+++ src/primerpython.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "JO-RArmas"
+__date__ = "$29-sep-2015 23:09:28$"
+
+if __name__ == "__main__":
+    print "Hello World"
